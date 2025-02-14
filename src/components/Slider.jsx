97f80fa2dedbc5df779e6e9d594d09ddcd4b7fc6
EmyNeauport <emilie.neauport@outlook.fr>
2025-02-14 17:26:26 +0100

    product page responsive

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,8 +1,10 @@
-import { useParams } from "react-router-dom";
+import { useLinkClickHandler, useParams } from "react-router-dom";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import { Carousel } from 'react-responsive-carousel';
 import data from "../../data.json";
-import "../assets/styles/Slider.scss"
+import "../assets/styles/slider.scss"
+import leftArrow from "../assets/arrow-left.svg"
+import rightArrow from "../assets/arrow-right.svg"
 
 function Slider() {
     const { id } = useParams()
@@ -10,10 +12,25 @@ function Slider() {
     
     return <Carousel 
         infiniteLoop 
-        showThumbs={false}>
-        
-        {/* statusFormatter pour changer le format des chiffres */}
-        
+        showThumbs={false}
+        statusFormatter={(currentItem, total) =>
+            total === 1 ? "" : `${currentItem}/${total}`}
+        renderArrowPrev={(useLinkClickHandler, hasPrev) =>
+            hasPrev && (
+                <button className="carousel__arrow carousel__arrow--prev" onClick={useLinkClickHandler}>
+                    <img src={leftArrow} alt=""/>
+                </button>
+            )
+        }
+        renderArrowNext={(useLinkClickHandler, hasNext) =>
+            hasNext && (
+                <button className="carousel__arrow carousel__arrow--next" onClick={useLinkClickHandler}>
+                    <img src={rightArrow} alt=""/>
+                </button>
+            )
+        }
+        >
+                
         {product.pictures.map((picture, index) => (
             <div key={index}>
                 <img src={picture} alt=""/>
